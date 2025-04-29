f33a1ff5ba706c5f766d4d6c7e40e18c7dcc0a02
afcrawford <125093311+afcrawford@users.noreply.github.com>
2025-04-29 17:06:21 -0400

    added some buttons to replay and go to home

--- final_project/final_project/scenes/winstate.gd
+++ final_project/final_project/scenes/winstate.gd
@@ -1,4 +1,4 @@
-extends Control
+extends Node2D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,5 +11,9 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_start_button_pressed():
+func _on_restart_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/level1.tscn")
+
+
+func _on_home_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/intro.tscn")
